862a46e5f93b96f047e5763bedb7da8649afaa0b
TruongHoang2004 <22028035@vnu.edu.vn>
2023-09-21 12:11:37 +0700

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+module application.potato {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens application.potato to javafx.fxml;
+    exports application.potato;
+}
