78a5760cdb0fc4245b8925614876fafb4654c8bb
NguyenHai321 <125852548+NguyenHai321@users.noreply.github.com>
2023-10-13 23:50:11 +0700

    update the .fxml files and Style.css

--- src/main/java/application/controller/DictionaryController.java
+++ src/main/java/application/controller/DictionaryController.java
@@ -3,6 +3,7 @@ package application.controller;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
 public class DictionaryController extends MenuController {
 
@@ -11,6 +12,12 @@ public class DictionaryController extends MenuController {
 
     @FXML
     private TextArea resultArea = new TextArea();
+
+    @FXML
+    private TextArea recommendedArea = new TextArea();
+
+    @FXML
+    private AnchorPane resultAnchorPane = new AnchorPane();
 
     public void search() {
         resultArea.setText(searchField.getText());
